4ea96b4541f8436b2e9c2980744adee26636f58b
shahimerdan <shahim@aliyun.com>
2020-08-16 14:27:17 +0000

    用户注册页面

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -6,7 +6,7 @@
 	 <h1>Hello Laravel</h1>
 	 <p class="lead">你现在看到的是<a href="http://baidu.com">的</a>首页</p>
 	 <p>一切从这里开始</p>
-	 <p><a class="btn btn-lg btn-success" href="#" role="button">现在注册</a></p>
+    <p><a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a></p>
 	</div>
 
 @stop
