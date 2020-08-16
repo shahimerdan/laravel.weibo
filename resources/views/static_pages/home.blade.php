d52fe91f5c8ddcbcc92ff78fc0f3dc13e2f34941
shahimerdan <shahim@aliyun.com>
2020-08-16 11:17:24 +0000

    初始化样式

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,11 @@
 @section('title','主页')
 
 @section('content')
-<h1>主页</h1>
+	<div class="jumbotron">
+	 <h1>Hello Laravel</h1>
+	 <p class="lead">你现在看到的是<a href="http://baidu.com">的</a>首页</p>
+	 <p>一切从这里开始</p>
+	 <p><a class="btn btn-lg btn-success" href="#" role="button">现在注册</a></p>
+	</div>
+
 @stop
