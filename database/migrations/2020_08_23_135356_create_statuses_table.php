7bc5ab701d753462df8a8518675dd8caa8ef5511
shahimerdan <shahim@aliyun.com>
2020-08-23 15:06:36 +0000

    微博数据模型

--- database/migrations/2020_08_23_135356_create_statuses_table.php
+++ database/migrations/2020_08_23_135356_create_statuses_table.php
@@ -14,7 +14,10 @@ class CreateStatusesTable extends Migration
     public function up()
     {
         Schema::create('statuses', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->text('content');
+            $table->integer('user_id')->index();
+            $table->index(['created_at']);
             $table->timestamps();
         });
     }
