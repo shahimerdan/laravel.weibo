7df9c219f24fe34b7fd72b0321b8d4b9cd4e5688
shahimerdan <shahim@aliyun.com>
2020-08-23 09:33:16 +0000

    查看用户列表

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,14 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        Model::unguard();
+        $this->call(UsersTableSeeder::class);
+        Model::reguard();
     }
 }
